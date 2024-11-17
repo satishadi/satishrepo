c8cb84e1fde2e0696ccc799e9cac2c9f1b0a88e9
satishadi124@gmai.com <satishadi124@gmail.com>
2024-11-17 21:58:37 +0530

    satishc1

---
+++ src/main/java/com/check/Check1Application.java
@@ -0,0 +1,13 @@
+package com.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Check1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Check1Application.class, args);
+	}
+
+}
